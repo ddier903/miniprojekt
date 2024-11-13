6911f6a4c1189f1b727da38a017f31f9d54f17a5
ddier903 <victorot@hotmail.dk>
2024-11-13 13:29:26 +0100

    gfdf

--- Core1/Category.cs
+++ Core1/Category.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 
 
