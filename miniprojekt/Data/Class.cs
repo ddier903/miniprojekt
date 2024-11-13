1979d74886a75adde75631dd62f0b970ea9ab2c2
ddier903 <victorot@hotmail.dk>
2024-11-13 10:12:21 +0100

    fix

---
+++ miniprojekt/Data/Class.cs
@@ -0,0 +1,16 @@
+﻿using miniprojekt.Klasser;
+using MongoDB.Driver;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+    public IMongoCollection<Product> Products => _database.GetCollection<Product>("Products");
+}
