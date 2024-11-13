47707a481040ae10ca7206398f16c93abbfcfba3
ddier903 <victorot@hotmail.dk>
2024-11-13 13:41:39 +0100

    swagger

--- ServerAPI1/Program.cs
+++ ServerAPI1/Program.cs
@@ -1,25 +1,30 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using ServerAPI1.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MongoDB settings fra appsettings.json
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+
+// Registrere MongoDB client
+builder.Services.AddSingleton<IMongoClient>(sp =>
+    new MongoClient(
+        sp.GetRequiredService<IOptions<MongoDBSettings>>().Value.ConnectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
