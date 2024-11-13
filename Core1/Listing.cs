4ed27b175c6a42815719af6ca875df1b70873cf7
Frede52 <frederikkarno@gmail.com>
2024-11-13 10:31:13 +0100

    Added Listing and User class

---
+++ Core1/Listing.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Core1
+{
+    public class Listing
+    {
+        public ObjectId ListingID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        public bool IsActive { get; set; } = true;
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime ListingDate { get; set; } = DateTime.Now;
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+    }
+}
---
+++ Core1/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core1
+{
+    public class User
+    {
+        public ObjectId UserID { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        private string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public List<Listing> Listings { get; set; } = new List<Listing>();
+        public List<Order> Orders { get; set; } = new List<Order>();
+
+        public User(string username, string password, string firstname, string lastname, string email)
+        {
+            UserName = username;
+            Password = password;
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+        }
+
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+
+        public void AddListing(Listing listing)
+        {
+            Listings.Add(listing);
+        }
+    }
+}
